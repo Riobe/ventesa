a7a361debd35998d44a77bbd3785afd75d42a815
riobe <jeremy.pridemore@gmail.com>
2020-08-08 00:51:48 -0600

    Add an initial set of tests to Attribute.

--- src/components/Attribute/Attribute.spec.js
+++ src/components/Attribute/Attribute.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Attribute from './Attribute';
 
 describe('Attribute', () => {
@@ -26,5 +26,43 @@ describe('Attribute', () => {
     expect(label).toHaveStyle(`
       font-weight: 800;
     `);
+  });
+
+  it('should respond with the new value when clicking a dot.', async () => {
+    const testAttribute = 'testing';
+    const onValueChange = jest.fn();
+
+    const { getAllByRole } = render(
+      <Attribute name={testAttribute} onValueChange={onValueChange} />,
+    );
+
+    const dots = getAllByRole('radio');
+    dots.forEach((dot, i) => {
+      fireEvent.click(dot);
+      expect(onValueChange).toHaveBeenCalledWith(i + 1);
+    });
+  });
+
+  it('should check the right amount of dots based on value.', async () => {
+    const testAttribute = 'testing';
+    const testValue = 3;
+    const onValueChange = jest.fn();
+
+    const { getAllByRole } = render(
+      <Attribute
+        name={testAttribute}
+        value={testValue}
+        onValueChange={onValueChange}
+      />,
+    );
+
+    const dots = getAllByRole('radio');
+    dots.forEach((dot, index) => {
+      if (index < testValue) {
+        expect(dot).toBeChecked();
+      } else {
+        expect(dot).not.toBeChecked();
+      }
+    });
   });
 });
