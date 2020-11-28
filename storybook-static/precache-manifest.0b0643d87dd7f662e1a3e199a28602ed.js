6fe08fa2c732cd623d5ff43b4b98ea911147f0a6
riobe <jeremy.pridemore@gmail.com>
2020-11-27 22:12:28 -0700

    Add static storybook site.

---
+++ storybook-static/precache-manifest.0b0643d87dd7f662e1a3e199a28602ed.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e3346d1dea0a9f1459be7a29814604",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.dee1a847fc3a26015dd7.bundle.js"
+  },
+  {
+    "url": "runtime~main.dee1a847fc3a26015dd7.bundle.js"
+  },
+  {
+    "url": "vendors~main.dee1a847fc3a26015dd7.bundle.js"
+  },
+  {
+    "url": "vendors~main.dee1a847fc3a26015dd7.bundle.js.LICENSE.txt"
+  }
+]);
