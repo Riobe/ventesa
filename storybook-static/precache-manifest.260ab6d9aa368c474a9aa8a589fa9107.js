f922bd9270dde23fad62fa0f9d79f7063bcf54d0
riobe <jeremy.pridemore@gmail.com>
2020-11-28 09:24:51 -0700

    Update storybook-static with adding a component.

---
+++ storybook-static/precache-manifest.260ab6d9aa368c474a9aa8a589fa9107.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d30bd55da4e621a297016ce86f61c46",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.79a86293a99a7d4e4fd6.bundle.js"
+  },
+  {
+    "url": "runtime~main.79a86293a99a7d4e4fd6.bundle.js"
+  },
+  {
+    "url": "vendors~main.79a86293a99a7d4e4fd6.bundle.js"
+  },
+  {
+    "url": "vendors~main.79a86293a99a7d4e4fd6.bundle.js.LICENSE.txt"
+  }
+]);
