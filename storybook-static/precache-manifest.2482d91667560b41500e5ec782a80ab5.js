62a78eedd275c69c712d856b5a4fdcf5bcc3824a
riobe <jeremy.pridemore@gmail.com>
2020-11-30 22:27:03 -0700

    Static build of first storybook full documentation.

---
+++ storybook-static/precache-manifest.2482d91667560b41500e5ec782a80ab5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72415b567be59e8ce03313212ea9372",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e994e0728e857f5d931b.bundle.js"
+  },
+  {
+    "url": "runtime~main.e994e0728e857f5d931b.bundle.js"
+  },
+  {
+    "url": "vendors~main.e994e0728e857f5d931b.bundle.js"
+  },
+  {
+    "url": "vendors~main.e994e0728e857f5d931b.bundle.js.LICENSE.txt"
+  }
+]);
