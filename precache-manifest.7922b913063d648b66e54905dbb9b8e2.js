b0f71114c8b325a4f857acaa27a730b789d1ce55
riobe <jeremy.pridemore@gmail.com>
2020-11-30 22:46:25 -0700

    sb-build: Fix typo, lack of spaces, causing a missing newline.

---
+++ precache-manifest.7922b913063d648b66e54905dbb9b8e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80346283d13adc568d174ffadb48fa06",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bd1cfc3c7b6989acd761.bundle.js"
+  },
+  {
+    "url": "runtime~main.bd1cfc3c7b6989acd761.bundle.js"
+  },
+  {
+    "url": "vendors~main.bd1cfc3c7b6989acd761.bundle.js"
+  },
+  {
+    "url": "vendors~main.bd1cfc3c7b6989acd761.bundle.js.LICENSE.txt"
+  }
+]);
